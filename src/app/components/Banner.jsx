4c9e446cd781d0f4533b822c66f1e76fbcb4298c
RINITH-AMIN <rinith.amin333@gmail.com>
2024-03-26 20:26:29 +0530

    initial commits

---
+++ src/app/components/Banner.jsx
@@ -0,0 +1,105 @@
+"use client";
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+import bannerData from "../../../public/data/bannerData.json";
+
+export const Banner = () => {
+  const { textName, textWelcome, textAbout, textLetsConnect } = bannerData;
+
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [role, setRole] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = ["Oracle Commerce Cloud Developer.", "FullStack Developer."];
+  // Time freez for the text after animation
+  const period = 3000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [role]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedRoleText = isDeleting
+      ? fullText.substring(0, role.length - 1)
+      : fullText.substring(0, role.length + 1);
+
+    setRole(updatedRoleText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedRoleText === fullText) {
+      setIsDeleting(true);
+      setIndex((prevIndex) => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedRoleText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  return (
+    <section className="banner" id="home">
+      <video autoPlay muted loop id="bg-video">
+        <source src="/img/bannerBackgroundVideo.mp4" type="video/mp4" />
+      </video>
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <span className="tagline">{textWelcome}</span>
+                  <h1>{textName} </h1>
+                  <h2>
+                    <span className="txt-rotate">
+                      <span className="wrap">{role}</span>
+                    </span>
+                  </h2>
+                  <p>{textAbout}</p>
+                  <button onClick={() => console.log("connect")}>
+                    {textLetsConnect} <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                >
+                  <img src="/img/bannerSideImage.svg" alt="Header Img" />
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
