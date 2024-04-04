0428c107d919a20007ab576cbd04f61bf72e1863
RINITH-AMIN <rinith.amin333@gmail.com>
2024-04-04 20:18:36 +0530

    SEO related changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Rinith Amin",
   description: "Portfolio of Rinith Amin",
+  keywords: ["rinith,rinithamin,rinith amin,RINITH,RINITH AMIN"],
+  author: "Rinith Amin",
+  og: {
+    title: "Rinith Amin - Portfolio",
+    description: "Check out the portfolio of Rinith Amin.",
+    image: "https://rinithamin.in/img/website-previewImage.png",
+    robots: "index, follow"
+  },
 };
 
 export default function RootLayout({ children }) {
